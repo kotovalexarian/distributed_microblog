fbf418d516154755a84ddd829d877cccd87edf1d
Alex Kotov <kotovalexarian@gmail.com>
2021-03-06 05:50:38 +0500

    Add action AddressesController#show

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :addresses, only: :index
+  resources :addresses, only: %i[index show]
 
   resources :profiles, only: %i[index show] do
     resources :posts, shallow: true, only: %i[new create]
