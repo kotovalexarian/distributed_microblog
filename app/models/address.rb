3e6e2ba6f5f1d8e604315aa45a2c8fe374bdbbb5
Alex Kotov <kotovalexarian@gmail.com>
2021-03-09 11:50:25 +0500

    Add scopes Address.added_to_wallet, .to_update_op_return

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,13 @@
 class Address < ApplicationRecord
+  scope :added_to_wallet, -> { where added_to_wallet: true }
+
+  scope :to_update_op_return, lambda {
+    added_to_wallet.where(
+      Address.arel_table[:op_return_value].eq(nil)
+        .or(Address.arel_table[:op_return_checked_at].lt(1.hour.ago)),
+    )
+  }
+
   validates :value, presence: true
 
   after_create :add_to_dash_wallet
